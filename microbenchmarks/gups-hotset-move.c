7b42cc5b7afda86dbeaf84c9542283ae54941c8a
root <root@localhost>
2025-09-21 12:14:02 -0600

    changed gups-hotset-move to work without hemem .so file

--- microbenchmarks/gups-hotset-move.c
+++ microbenchmarks/gups-hotset-move.c
@@ -35,7 +35,7 @@
 #include <stdbool.h>
 
 #include "../src/timer.h"
-#include "../src/hemem.h"
+// #include "../src/hemem.h"
 
 
 #include "gups.h"
@@ -259,7 +259,7 @@ int main(int argc, char **argv)
   fprintf(stderr, "field of 2^%lu (%lu) bytes\n", expt, size);
   fprintf(stderr, "%ld byte element size (%ld elements total)\n", elt_size, size / elt_size);
 
-  p = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS | MAP_HUGETLB | MAP_POPULATE, -1, 0);
+  p = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS | MAP_POPULATE, -1, 0);
   if (p == MAP_FAILED) {
     perror("mmap");
     assert(0);
